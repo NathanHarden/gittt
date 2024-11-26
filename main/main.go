bb8ef0085d83eed1ba3ba09c62b5f923fcfd550d
NathanHarden <wenweisen167@gmail.com>
2024-11-26 19:39:01 +0800

    alan1

--- main/main.go
+++ main/main.go
@@ -5,9 +5,9 @@ import (
 	"gittt"
 )
 func main(){
-	a:=99
+	a:=80
 	fmt.Printf("%d\n",a)
-	b:=11111
-	c:=888
-	fmt.Printf("%d\n",gittt.Sum(gittt.Sum(a,b),c))
+	b:=27
+	c:=41
+	fmt.Printf("%d\n",gittt.Sum(gittt.Sum(c,b),a))
 }
